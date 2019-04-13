caac5c2aa7ea38a8467337666a911bcbc9c678d8
auredentan <aurelien.dentan@gmail.com>
2019-04-13 22:26:11 +0200

    render react

--- simple-webapp/server/app.py
+++ simple-webapp/server/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder=build_dir, static_folder=static_dir)
 
 @app.route('/')
 def hello_world():
-    return 'hello_world'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
