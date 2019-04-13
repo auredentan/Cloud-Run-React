f6bdee9d39a2073a6fefe074fca9b67cf994c890
auredentan <aurelien.dentan@gmail.com>
2019-04-13 22:40:50 +0200

    Deploy to prod

--- simple-webapp/client/src/App.js
+++ simple-webapp/client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Prod
+            Learn React Prod 1
           </a>
         </header>
       </div>
